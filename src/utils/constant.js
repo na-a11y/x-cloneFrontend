5f0d29414a6d43f2364d586249f636cce088773e
na-a11y <navjotmanak01@gmail.com>
2024-12-01 22:45:18 +0530

    after deployment

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:5000/api/v1/user";
-export const TWEET_API_END_POINT = "http://localhost:5000/api/v1/tweet";
+export const USER_API_END_POINT = "https://x-clonebackend.onrender.com/api/v1/user";
+export const TWEET_API_END_POINT = "https://x-clonebackend.onrender.com/api/v1/tweet";
 
 export const timeSince = (timestamp) => {
     let time = Date.parse(timestamp);
